345fddff20ef204ad288548a242e31ceb7f42010
Carrie <cerobi1@ilstu.edu>
2018-08-28 16:14:43 -0500

    Made GUI layout, created controller class

--- src/it226cerobi1/IT226cerobi1.java
+++ src/it226cerobi1/IT226cerobi1.java
@@ -3,11 +3,8 @@
  */
 package it226cerobi1;
 
-import java.util.*;
-
 public class IT226cerobi1 {
 
-    public static Scanner userIn = new Scanner(System.in); //will hold get user input
     public static void main(String[] args) {
         
         
---
+++ src/it226cerobi1/It226cerobi1Controller.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it226cerobi1;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author Carrie
+ */
+public class It226cerobi1Controller implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+        
+        
+    }    
+    
+}
