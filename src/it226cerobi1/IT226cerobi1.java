2f27b76bf67aa9b7fd7a7b3190f26f410c13e96e
Carrie <cerobi1@ilstu.edu>
2018-08-28 15:34:10 -0500

    Just kidding, now I added a scanner

--- src/it226cerobi1/IT226cerobi1.java
+++ src/it226cerobi1/IT226cerobi1.java
@@ -1,20 +1,16 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This program will be a simple 4 function calculator app
  */
 package it226cerobi1;
 
-/**
- *
- * @author Carrie
- */
+import java.util.*;
+
 public class IT226cerobi1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static Scanner userIn = new Scanner(System.in); //will hold get user input
     public static void main(String[] args) {
+        
+        
         
     }
     
