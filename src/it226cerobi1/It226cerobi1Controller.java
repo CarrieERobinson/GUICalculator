3b9fca843cdc975e86474f0e48ea3ebb0e62983e
Carrie <cerobi1@ilstu.edu>
2018-09-03 10:51:04 -0500

    Added solve function, pushing to functions for real this time

--- src/it226cerobi1/It226cerobi1Controller.java
+++ src/it226cerobi1/It226cerobi1Controller.java
@@ -6,10 +6,9 @@
 package it226cerobi1;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.Stack;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -21,6 +20,9 @@ public class It226cerobi1Controller implements Initializable {
     /**
      * Initializes the controller class.
      */
+    public static Stack<Character> operatorStack = new Stack<>();
+    public static Stack<Double> operandStack = new Stack<>();
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
@@ -30,23 +32,80 @@ public class It226cerobi1Controller implements Initializable {
         
     }    
     
-    
-    public static double solveEq(String equation)
+//========================solve()===============================================
+    //will interpret and solve an equation in string form; prints and returns the answer
+    public static double solve(String equation)
     {
-        double solution;
-        equation = equation.replace("//s", "");
-        String operands[] = new String[equation.length()];
-        
+        equation.replaceAll("//s", ""); //remove any spaces
+        //iterate through the equation string
         for(int i = 0; i < equation.length(); i++)
         {
-            //
-            if(i == 0 && Character.isAlphabetic(equation.charAt(i)))
+            //check if char is a digit, if so, check for how many digits the
+            //number has and push them to the stack.
+            if(Character.isDigit(equation.charAt(i)))
             {
+                int j = i; //new index to search ahead in the string
+                String num = ""; //this string will hold the digits in the number to be pushed later
+                
+                //continue through equatiion until operand or end of string is reached
+                while(j < equation.length() && Character.isDigit(equation.charAt(j)))
+                {
+                    num += equation.charAt(j);//add number to string
+                    j++; //increment j
+                }
+                
+                i = j - 1; //update i so that it is in the correct index after the loop
+                double finalNum = Double.parseDouble(num); //parse string into a double
+                operandStack.push(finalNum); //push that number onto the stack
+            }else
+            {
+                //this checks that the first char is not non-numerical
+                if(i == 0 || i == equation.length() - 1)
+                {
+                    System.out.println("Invalid format detected!");
+                    return 0;
+                }else if(!Character.isDigit(equation.charAt(i-1)))
+                {
+                    //checks that 2 operands are not put in a row
+                    System.out.println("Invalid format detected!");
+                    return 0;
+                }
+                
+                //in case of a non-numerical character, check for an operator
+                if(equation.charAt(i) == '+' || equation.charAt(i) == '-' || 
+                equation.charAt(i) == '*' || equation.charAt(i) == '/')
+                {
+                    //check if the previous operator needs to be evalutated
+                    //(* or / when current is + or -)
+                    while(!operatorStack.isEmpty() && 
+                            (operatorStack.peek() == '*' || operatorStack.peek() == '/')
+                            && (equation.charAt(i) == '-' || equation.charAt(i) == '+'))
+                    {
+                        process(); //evaluate previous operator so that order of
+                        //operations is upheld, will continue until + or - or empty stack
+                    }
+                    operatorStack.push(equation.charAt(i));
+                }else
+                {
+                    //the character is invalid!
+                    System.out.println("Invalid character detected!");
+                    return 0;
+                }
                 
             }
         }
         
+        while(!operatorStack.isEmpty())
+        {
+            process(); //process remaining operations until the stack is empty
+        }
         
-        return solution;
+        System.out.println(operandStack.peek());
+        return operandStack.pop();
+    }
+    
+    public static void process()
+    {
+        
     }
 }
