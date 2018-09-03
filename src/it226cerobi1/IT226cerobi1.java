4dd2a0d9abde675b4aa4dcd25ba540b3697b1b39
Carrie <cerobi1@ilstu.edu>
2018-09-03 11:25:14 -0500

    Added button pressing w/ text for nums and operators

--- src/it226cerobi1/IT226cerobi1.java
+++ src/it226cerobi1/IT226cerobi1.java
@@ -3,12 +3,29 @@
  */
 package it226cerobi1;
 
-public class IT226cerobi1 {
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
+public class IT226cerobi1 extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("it226cerobi1.fxml"));
         
+        Scene scene = new Scene(root);
         
-        
+        stage.setScene(scene);
+        stage.show();
     }
-    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/it226cerobi1/It226cerobi1Controller.java
+++ src/it226cerobi1/It226cerobi1Controller.java
@@ -6,10 +6,12 @@
 package it226cerobi1;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -21,12 +23,26 @@ public class It226cerobi1Controller implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    @FXML
+    TextField resultBox;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         
         
-        
     }    
+    
+    @FXML
+    public void onButtonClick(ActionEvent event)
+    {
+        if(event.getSource() instanceof Button)
+        {
+            Button button = (Button)event.getSource();
+            //add button text to screen text
+            resultBox.setText(resultBox.getText().trim() + button.getText().trim());
+        }
+    }
     
 }
