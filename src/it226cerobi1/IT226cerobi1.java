75a8e7d669b9b596692d2bcc7027370c07ef814e
Carrie <cerobi1@ilstu.edu>
2018-09-03 12:32:40 -0500

    Added window title

--- src/it226cerobi1/IT226cerobi1.java
+++ src/it226cerobi1/IT226cerobi1.java
@@ -18,6 +18,7 @@ public class IT226cerobi1 extends Application{
         
         Scene scene = new Scene(root);
         
+        stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
     }
